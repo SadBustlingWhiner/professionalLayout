9768866c1d8794cdc84f1821b5d3a58cf254d8d1
Annet <hlanastasiya@mail.ru>
2021-10-22 21:34:39 +0300

    sass

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+let button = document.getElementById('burger');
+
+let navigation = document.getElementById('menu');
+
+// let wrp = document.getElementById('wrp');
+
+button.addEventListener('click', function(){
+    navigation.classList.toggle('header__nav--show');
+    // wrp.classList.toggle('display-block');
+})
