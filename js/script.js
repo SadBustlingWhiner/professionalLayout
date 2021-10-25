c12d8ad3352535d5ddc7e3b1826a0d95f059fc3b
Annet <hlanastasiya@mail.ru>
2021-10-25 21:15:49 +0300

    mobile version

--- js/script.js
+++ js/script.js
@@ -10,3 +10,11 @@ button.addEventListener('click', function(){
     navigation.classList.toggle('header__nav--show');
     // wrp.classList.toggle('display-block');
 })
+
+// let card = document.getElementById('card');
+
+// let cart = document.getElementById('to_cart');
+
+// card.addEventListener('click', function(){
+//     cart.classList.toggle('catalog__item-cart');
+// })
