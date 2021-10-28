a367041d211e13a4ef2084a09cf7330603883a35
Annet <hlanastasiya@mail.ru>
2021-10-28 08:15:28 +0300

    upd styles

--- js/script.js
+++ js/script.js
@@ -18,3 +18,16 @@ button.addEventListener('click', function(){
 // card.addEventListener('click', function(){
 //     cart.classList.toggle('catalog__item-cart');
 // })
+
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel)
+var myCarousel = document.querySelector('#myCarousel')
+var carousel = new bootstrap.Carousel(myCarousel, {
+  interval: 2000,
+  wrap: false
+})
+var myCarousel = document.getElementById('myCarousel')
+
+myCarousel.addEventListener('slide.bs.carousel', function () {
+  // do something...
+})
